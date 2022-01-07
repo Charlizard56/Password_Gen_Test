3202f4f5bf17e128961549f785feb280d9205a2a
DM <85085855+Charlizard56@users.noreply.github.com>
2022-01-06 19:08:13 -0500

    + Can Check if Symbols are used

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import Gen
 import List_and_tuples
-
+import itertools
 t = False
-times = 6
+times = 3
 
 if __name__ == '__main__':
     # Generate password
     Gen.gen(times, t)
 
     print(List_and_tuples.Password)
+    for p, s in itertools.product(List_and_tuples.Password,List_and_tuples.Symbols):
+        if p == s:
+            print("True")
