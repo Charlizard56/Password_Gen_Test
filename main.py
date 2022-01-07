25fbe63c2a9eede5cdc6a1bea27c3bb3126b5e01
DM <85085855+Charlizard56@users.noreply.github.com>
2022-01-06 22:16:45 -0500

    + Added Upper Check

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import Gen
 import List_and_tuples
 import itertools
-#update
+
 t = False
 times = 8
 symbol = False
 number = False
+letters_lower = False
+letters_upper = False
 
 if __name__ == '__main__':
     # Generate password
@@ -13,11 +15,19 @@ if __name__ == '__main__':
 
     print(List_and_tuples.Password)
     # Compare the password list to the other tuples
-    for p, s, n in itertools.product(List_and_tuples.Password, List_and_tuples.Symbols, List_and_tuples.Numbers):
+    for p, s, n, l, L in itertools.product(List_and_tuples.Password, List_and_tuples.Symbols, List_and_tuples.Numbers,
+                                        List_and_tuples.letters, List_and_tuples.Letters):
         # Check if there is at least 1 Symbol
         if p == s:
             symbol = True
         # Check if there is at least 1 Number
         if p == n:
             number = True
-    print(f'\nSymbol: {symbol} Number: {number}')
+        if p == l:
+            letters_lower = True
+        if p == L:
+            letters_upper = True
+
+
+
+    print(f'\nSymbol: {symbol} | Number: {number} | Lowercase: {letters_lower}| Upper: {letters_upper}')
